e036a11b0ced545c69801a3f27869d1c1b71ff68
Andrew <andrew.ku93@gmail.com>
2021-03-02 22:24:25 +0000

    create multiple queries for assistance request data

---
+++ 4_queries/5_get_cohort_with_longest_request_avg.sql
@@ -0,0 +1,11 @@
+SELECT cohorts.name AS cohort, avg(completed_at - started_at) AS average_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohort
+ORDER BY average_assistance_time DESC
+LIMIT 1;
+
+
+
+
